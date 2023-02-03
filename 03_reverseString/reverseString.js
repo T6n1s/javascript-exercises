e16f2ea68ca3e8a0ad92ef040d64e43059643b43
T6n1s <t6nis.paluoja@gmail.com>
2023-02-03 08:52:52 +0200

    Complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const instr = string.split("");
+    let backstr = instr.reverse();
+    backstr = backstr.join("");
+    return backstr;
+    
 };
 
 // Do not edit below this line
