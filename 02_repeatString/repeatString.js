9ead9c0ab5a2fbd794bcf648ab87f0cfe1ec9fa2
T6n1s <t6nis.paluoja@gmail.com>
2023-02-03 08:23:42 +0200

    Complete 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let str = string;
+    if (n > 0) {
+    for (let x = 1; x < n; x++) {      
+        str += string; 
+    } return str;
+} else if (n === 0) {
+    return "";
+} else if (n < 0) {
+    return "ERROR";
+}
 };
 
 // Do not edit below this line
