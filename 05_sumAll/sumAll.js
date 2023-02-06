2ea9514efa99627a4434ae84ef174a1437fe6e2f
T6n1s <t6nis.paluoja@gmail.com>
2023-02-06 15:19:28 +0200

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
+    let o = a;
+    let u = b;
 
-};
+    if (a < 0 || typeof b === "string" || b instanceof Array) {
+        return ("ERROR")
+    } else if (b < a) {
+        for (let i = 0; i < a; i++) {
+            sum += u;
+            u++;
+        }
+    } else {
+    for (let i = 0; i < b; i++) {
+        sum += o;
+        o++;
+    } 
+    } return sum;
+} ;
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
