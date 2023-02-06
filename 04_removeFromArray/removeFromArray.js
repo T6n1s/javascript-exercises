65272b264f55df08bb699eab9c2b51a74739cd05
T6n1s <t6nis.paluoja@gmail.com>
2023-02-06 14:46:22 +0200

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
+    const removeFromArray = function(a, ...b) {
+        let removed = a.filter((x) => !b.includes(x));
+        return removed;
+    }
 
-};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
