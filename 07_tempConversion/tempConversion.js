c713258b202333fffa7f10da12e70b71123f86ad
T6n1s <t6nis.paluoja@gmail.com>
2023-02-06 15:45:09 +0200

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(c) {
+  let f = (c - 32) * .5556;
+  let rounded = Math.round(f * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(f) {
+  let c = f * 1.8 + 32;
+  let rounded = Math.round(c * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
